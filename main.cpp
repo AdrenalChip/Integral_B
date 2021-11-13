dd1b505c811f2ba078383a0b13df5e744011d0d3
Jorge Guerrero Díaz <74365148+AdrenalChip@users.noreply.github.com>
2021-11-13 12:45:16 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,7 +29,23 @@ void create(){
     fout.close();
 }
 
-
+void read(){
+    for (int i=0;i< 1;i++){
+		string line;
+		ifstream lee("Viajes.csv");   
+		string u,v;
+		if (lee.is_open()){
+			while(getline(lee,line)){
+				u=line;
+				v=line;
+                cout<<u<<" "<<v;
+			}
+			lee.close();
+		}else{
+			cout<<"Unable to open file";
+		}
+	}
+}
 int main(){
     //create();
     Graph g(4);
@@ -38,4 +54,5 @@ int main(){
     g.addEdge("D","E",7);
     g.addEdge("E","A",5);
     g.display();
+    read();
 }
