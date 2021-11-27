0cedc3a012816d58b8b631e01e3de3f387b2efbe
Jorge Guerrero Díaz <74365148+AdrenalChip@users.noreply.github.com>
2021-11-27 13:54:34 -0600

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -11,80 +11,151 @@
 #include <stack>
 #include <list>
 #include <algorithm>
-#include <map>
+
 using namespace std;
 
-
-class Graph {
+template <class Key, class Value>
+class Quadratic {
 private:
-	int V; 
-    map<pair<string,string>,int> city ;
-    vector<string> city1;
-    vector<string> city2;
-    map<int, bool> visited;
-    public:
-    Graph(int start_V)
-    {
-        V = start_V;
-    }
-    void addEdge(string start,string end,int wt);
-    void display();
-    void DFS(int v);
+	unsigned int (*func) (const Key);
+	unsigned int size;
+	unsigned int count;
+
+	Key *keys;
+	Key initialValue;
+	Value *values;
+
+	long indexOf(const Key) const;
+
+public:
+	Quadratic(unsigned int, Key, unsigned int (*f) (const Key));
+	~Quadratic();
+	bool full() const;
+	bool put(Key, Value);
+	bool contains(const Key) const;
+	Value get(const Key);
+	void clear();
+	std::string toString() const;
 };
 
-void Graph::addEdge(string start,string end,int wt)
-{   
-   city[make_pair(start,end)] = wt;
+template <class Key, class Value>
+Quadratic<Key, Value>::Quadratic(unsigned int sze, Key init, unsigned int (*f) (const Key)){
+	size = sze;
+	keys = new Key[size];
+	if (keys == 0) {
+		cout<<"OutOfMemory";
+	}
+	initialValue = init;
+	for (unsigned int i = 0; i < size; i++) {
+		keys[i] = init;
+	}
+	values = new Value[size];
+	if (values == 0) {
+		cout<< "OutOfMemory";
+	}
+	for (int i = 0; i  < sze; i++){
+        values[i] = 0;
+    }
+
+	func = f;
+	count = 0;
 }
 
-void Graph::display()
-{   
-    int flag=0;
-    for(auto it:city)
-    {
-        city1.push_back(it.first.first);
-        city2.push_back(it.first.second);
-    }
-
-    sort(city1.begin(), city1.end()); 
-    auto last = unique(city1.begin(), city1.end());
-    city1.erase(last, city1.end());
-
-    sort(city2.begin(), city2.end()); 
-    auto last2 = unique(city2.begin(), city2.end());
-    city2.erase(last2, city2.end());
-
-    for(auto col:city2)
-    {
-        cout<<"\t"<<col;
-    }
-    cout<<"\n";
-
-    for(auto row:city1)
-    {
-    int flag =0;   
-    for(auto col:city2)
-    {
-        if (!flag)
-            cout<<row;
-        cout<<"\t"<<city[make_pair(row,col)];
-        flag = 1;
-    }
-    cout<<"\n";
-    }
-
+template <class Key, class Value>
+Quadratic<Key, Value>::~Quadratic() {
+	delete [] keys;
+	keys = 0;
+	delete [] values;
+	values = 0;
+	size = 0;
+	func = 0;
+	count = 0;
 }
-/*
-void Graph::DFS(int v)
-{
-    city.find("v");
 
-    visited[v] = true;
-    cout << v << " ";
+template <class Key, class Value>
+bool Quadratic<Key, Value>::full() const {
+	return (count > size);
+}
 
-    list<int>::iterator i;
-    for (i = city[v].begin(); i != city[v].end(); ++i)
-        if (!visited[*i])
-            DFS(*i);
-}*/
-#endif
+template <class Key, class Value>
+long Quadratic<Key, Value>::indexOf(const Key k) const {
+	unsigned int i, start;
+
+	start = i = func(k) % size;
+	do {
+		if (keys[i] == k) {
+			return i;
+		}
+		i = (i + 1) % size;
+	} while (start != i);
+	return -1;
+}
+
+template <class Key, class Value>
+bool Quadratic<Key, Value>::put(Key k, Value v){
+	unsigned int i, start;
+	long pos;
+
+	if (full()) {
+		cout<< "Overflow";
+	}
+
+	pos = indexOf(k);
+	if (pos != -1) {
+		values[pos] = v;
+		return true;
+	}
+
+	start = i = func(k) % size;
+	do {
+		if (keys[i] == initialValue) {
+			keys[i] = k;
+			values[i] = v;
+			count++;
+			return true;
+		}
+		i = (i + 1) % size;
+	} while (start != i);
+	return false;
+}
+
+template <class Key, class Value>
+bool Quadratic<Key, Value>::contains(const Key k) const {
+	long pos;
+
+	pos = indexOf(k);
+	return (pos != -1);
+}
+
+template <class Key, class Value>
+Value Quadratic<Key, Value>::get(const Key k){
+	unsigned int i, start;
+	long pos;
+
+	pos = indexOf(k);
+	if (pos != -1) {
+		return values[pos];
+	}
+	cout<< "NoSuchElement";
+}
+
+template <class Key, class Value>
+void Quadratic<Key, Value>::clear() {
+	count = 0;
+	for (unsigned int i = 0; i < size; i++) {
+		keys[i] = initialValue;
+	}
+}
+
+template <class Key, class Value>
+std::string Quadratic <Key, Value>::toString() const {
+    stringstream aux;
+	for (int i = 0; i < size; i++){
+			if (keys[i] != initialValue){
+				aux << "(" << i << " ";
+			  aux << keys[i] << " : " << values[i] << ") ";
+			}
+	}
+	return aux.str();
+}
+#endif /* Graph_H_ */
